a630a83b401deeb45e1eb3139632a0d431fef752
mathiscode <code@mathis.network>
2026-01-20 21:37:39 -0600

    chore: sync version and docs

--- core/kernel/tests/tree/i18n.test.ts
+++ core/kernel/tests/tree/i18n.test.ts
@@ -4,6 +4,6 @@ import { I18n } from '#i18n.ts'
 describe('I18n', () => {
   it('should be able to get a translation', () => {
     const i18n = new I18n({ lng: 'en' })
-    expect(i18n.t('kernel.permissionNotificationDenied', { ns: 'kernel' })).toBe('Notification permission denied')
+    expect(i18n.t('permissionNotificationDenied', { ns: 'kernel' })).toBe('Notification permission denied')
   })
 })
